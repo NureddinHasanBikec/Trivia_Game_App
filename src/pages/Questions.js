597ccbeba6e152ca82b774d735cdbdf741474df5
NureddinHasanBikec <nureddinhasanbikec@gmail.com>
2021-03-07 16:14:25 +0300

    Designed Questions page, arranged  transitions

--- src/pages/Questions.js
+++ src/pages/Questions.js
@@ -1,30 +1,48 @@
-import React from 'react';
+import React, {useState, useRef} from 'react';
 import {View, 
-        Flatlist, 
+        FlatList,  
         SafeAreaView, 
         Text
         } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
 import { CountdownCircleTimer } from 'react-native-countdown-circle-timer'
+import { QuestionItem } from '../components';
 
 const Questions = (props) => {
 
+    const listRef = useRef(null)
+    const[currentIndex, setCurrentIndex] = useState(0);
     const questionList = useSelector( global => global.questions );
 
-    console.log(questionList)
+    const renderQuestion = ({item}) => (
+         <QuestionItem 
+            questionObject={item}
+            onAnswer={answer}   
+         />
+    )
+
+    const answer = (result) => {
+        const newIndex = currentIndex + 1;
+        
+        if(newIndex === questionList.length)
+        return props.navigation.navigate("Finish")
+
+        listRef.current.scrollToIndex({ index: newIndex })
+        setCurrentIndex(newIndex)
+    }
 
     return(
         <SafeAreaView style={{flex:1}}>
             <View style={{flex:1}}>
-              <Text>ques</Text>
-                {/* <FlatList
+              
+                <FlatList
                     ref= {listRef}
                     horizontal
                     scrollEnabled={false}
                     keyExtractor={(_,i) => i.toString()}
-                    data={questions}
-                    renderItem={renderQuestions}
-                /> */}
+                    data={questionList}
+                    renderItem={renderQuestion}
+                />
             </View>
         </SafeAreaView>
     );
